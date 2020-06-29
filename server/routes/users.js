3649eac5088beed4d569f565e5de409ea3345488
Brian <a.gray.cloud@gmail.com>
2020-06-29 12:52:55 -0400

    flesh out basic user registration

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,38 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+const validateRegisterInput = require("../validators/register");
+
 const { User } = require("../models");
+
+router.post("/register", async (req, res) => {
+  const hashPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPw = await bcrypt.hash(password, salt);
+
+    return hashedPw;
+  };
+
+  const { errors, isValid } = validateRegisterInput(req.body);
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const existingUser = await User.findOne({ where: { email: req.body.email } });
+
+  if (existingUser) {
+    return res.status(400).json({ email: "That email already exists" });
+  }
+
+  const newUser = await User.create({
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  newUser.password = await hashPassword(newUser.password);
+  await newUser.save();
+
+  res.json(newUser);
+});
 
 module.exports = router;
