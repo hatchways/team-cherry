4bc47138e26c4be83033f2dd072443d042537a2a
Brian <a.gray.cloud@gmail.com>
2020-06-29 17:23:01 -0400

    allow db connection to be dynamically config'd based on node env

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,16 @@
-const {Sequelize} = require('sequelize')
+const { Sequelize } = require("sequelize");
+
+const dbName =
+  process.env.NODE_ENV === "test"
+    ? "test-db"
+    : process.env.DB_NAME || "db name not set";
 
 const db = new Sequelize(
-    process.env.DB_NAME || 'db name not set',
-    process.env.DB_USERNAME || 'postgres',
-    process.env.DB_PASSWORD || 'password',
-    {host: 'localhost', dialect: 'postgres'});
+  //   process.env.DB_NAME || "db name not set",
+  dbName,
+  process.env.DB_USERNAME || "postgres",
+  process.env.DB_PASSWORD || "password",
+  { host: "localhost", dialect: "postgres" }
+);
 
-module.exports = db
+module.exports = db;
