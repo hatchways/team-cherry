f4bcc5868839164c78fa5347a3eb4a64f7d37732
Brian <a.gray.cloud@gmail.com>
2020-06-29 14:21:54 -0400

    move cookie config to separate file

--- server/app.js
+++ server/app.js
@@ -10,11 +10,6 @@ const authRoutes = require("./routes/users");
 
 const { json, urlencoded } = express;
 
-const cookieConfig = {
-  httpOnly: true,
-  maxAge: 31556926, //1 yr
-};
-
 var app = express();
 
 app.use(logger("dev"));
