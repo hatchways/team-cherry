b9e7dd042ef8c02f7cad8b5f6643c40acbb2d929
Brian <a.gray.cloud@gmail.com>
2020-06-29 16:14:36 -0400

    modify middleware to only send user id

--- server/routes/middleware/requiresAuth.js
+++ server/routes/middleware/requiresAuth.js
@@ -1,19 +1,19 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 module.exports = async (req, res, next) => {
   const create4xxResponse = (status, msg) =>
-    res.status(status).json({ error: msg });
-  const { token } = req.cookies;
+      res.status(status).json({error: msg});
+  const {token} = req.cookies;
   if (!token) {
-    return create4xxResponse(403, "Forbidden");
+    return create4xxResponse(403, 'Forbidden');
   } else {
     jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
       if (err) {
-        return create4xxResponse(401, "Unable to decode token");
+        return create4xxResponse(401, 'Unable to decode token');
       } else if (decodedToken.exp <= Date.now() / 1000) {
-        return create4xxResponse(401, "Token Expired");
+        return create4xxResponse(401, 'Token Expired');
       } else {
-        req.user = decodedToken;
+        req.user = {id: decodedToken.id};
         next();
       }
     });
