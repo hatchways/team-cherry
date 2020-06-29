cfc715b7f1a9712fcc4f276f91d600fcacac2101
Brian <a.gray.cloud@gmail.com>
2020-06-29 12:11:17 -0400

    register auth route to app

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
 const pingRouter = require("./routes/ping");
+const authRoutes = require("./routes/users");
 
 const { json, urlencoded } = express;
 
@@ -19,14 +20,15 @@ app.use(express.static(join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/ping", pingRouter);
+app.use("/api/users", authRoutes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
