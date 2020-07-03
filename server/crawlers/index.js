330f3c205874925639a6c136ee226974671bb3fd
powerseed5044 <powerseed5044@gmail.com>
2020-07-03 10:51:54 -0500

    Add a command to test scrapers in package.json
    Run "nodemon ./crawlers/index" to test scrapers.

--- server/crawlers/index.js
+++ server/crawlers/index.js
@@ -1,7 +1,14 @@
 const redditCrawler = require("./redditCrawler");
+require("dotenv").config();
 
 async function scrapeReddit(companyName, countOfPagesToScrape) {
   return await redditCrawler(companyName, countOfPagesToScrape);
 }
+
+async function test() {
+  let results = await scrapeReddit("", 1000);
+  console.log(results);
+}
+test();
 
 module.exports = { scrapeReddit };
