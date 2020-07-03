52ee5723b17fa36eaee6e02f086dd56ad23471aa
Brian <a.gray.cloud@gmail.com>
2020-07-03 13:30:09 -0400

    remove redundant attribute on conjunction table

--- server/models/userMentions.js
+++ server/models/userMentions.js
@@ -4,20 +4,7 @@ const User = require("./user");
 const Mention = require("./mention");
 
 const UserMentions = db.define("UserMentions", {
-  userId: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: User,
-      key: "id",
-    },
-  },
-  mentionId: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: Mention,
-      key: "id",
-    },
-  },
+  // extra auxilliliary attributes would be stored here
 });
 
 module.exports = UserMentions;
