f4541fa217a8be773479e2f0d44417873e3fc58f
Brian <a.gray.cloud@gmail.com>
2020-07-04 18:34:51 -0400

    have scraper placeholder return dudd data

--- server/scraper/index.js
+++ server/scraper/index.js
@@ -1,8 +1,9 @@
 const Scraper = require("./scraper");
 const scraper = new Scraper();
 
-module.exports = function callScraper() {
+module.exports = function callScraper(companyName, pagesToScrape) {
   // dudd function that would be calling the actual scraper;
   // import this into /server/bin/www and setInterval to periodically scrape
-  scraper.run();
+  let data = scraper.run(companyName, pagesToScrape);
+  return data;
 };
