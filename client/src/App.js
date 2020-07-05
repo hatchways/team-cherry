7a321c3f2d87e2da0e1080d446af7052973a73f9
John Tieu <johnson.tieu@gmail.com>
2020-07-05 16:46:53 -0400

    john/merged_login_branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MuiThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-
 import { theme } from "./themes/theme";
 import Signup from "./pages/Signup";
 import Login from './pages/Login'
 import Main from './pages/Main'
-import Header from "./pages/Header"
-import { eraseUser } from './utils/localStorage'
-import { loginInterceptor, AxiosInterceptor } from './utils/authAxios'
+import Header from "./components/Header"
+import { eraseUser, getUser } from './utils/localStorage'
+import { loginInterceptor, AxiosInterceptor, } from './utils/authAxios'
 import Snackbar from '@material-ui/core/Snackbar';
-import "fontsource-roboto";
 import CssBaseline from "@material-ui/core/CssBaseline";
-
 import "./App.css";
 
 function App() {
   const [open, setOpen] = useState(false);
   const [errorMessage, setErrorMessage] = useState('')
-  const [protectedRoutes, unhideProtectedRoutes] = useState(false)
+  const [protectedRoutes, unhideProtectedRoutes] = useState('')
+
   //adds an interceptor to check requests. If a request is sent with a 401, user is erased and send back to login
   const unauthorized = () => {
     eraseUser()
@@ -27,14 +25,18 @@ function App() {
     //user is erased, which should lead them back to login, they will not have access to routes if their localstorage is empty.Snackbar should also open up telling them what happened.
     console.log('logging out due to authorization failure')
   }
-
   const userLogin = () => {
-    unhideProtectedRoutes(true)
+    unhideProtectedRoutes('unhide')
+  }
+  const unsetProtectedRoutes = () => {
+    unhideProtectedRoutes('hide')
   }
 
-  const unsetProtectedRoutes = () => {
-    unhideProtectedRoutes(false)
-  }
+  useEffect(() => {
+    if (getUser()) {
+      unhideProtectedRoutes('unhide')
+    }
+  }, [])
   //this interceptor catches 401s
   AxiosInterceptor(unauthorized, unsetProtectedRoutes)
 
@@ -50,7 +52,7 @@ function App() {
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
           { /* routes should be inaccessible after here if token doesn't exist*/}
-          {protectedRoutes ?
+          {protectedRoutes === 'unhide' ?
             <Route exact path='/main' component={Main} />
             :
             <Redirect to="/signup" />}
