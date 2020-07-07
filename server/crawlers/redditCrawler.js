9999b4559da9170f977408fa48f239dc41c38c77
powerseed5044 <powerseed5044@gmail.com>
2020-07-07 11:12:01 -0500

    Update redditCrawler.js

--- server/crawlers/redditCrawler.js
+++ server/crawlers/redditCrawler.js
@@ -21,9 +21,9 @@ async function redditCrawler(companyName) {
     // console.log(post);
     // If a post's title doesn't contain the specified company name, or it doesn't have any content, just ignore it.
     if (post.title.includes(companyName) && post.selftext) {
-      // If a post doesn't have a thumbnail picture, just use Reddit's logo.
+      // If a post doesn't have a thumbnail picture, set its "thumbnail" to be null.
       if (post.thumbnail.substr(0, 8) !== "https://") {
-        post.thumbnail = "/imgs/reddit_icon.png";
+        post.thumbnail = null;
       }
       data.push({
         id: post.id,
