ee73b81b882f4992db6110f9149282332bd68ad3
John Tieu <johnson.tieu@gmail.com>
2020-07-08 20:30:23 -0400

    changed register route to return object w user as key:value, changed interceptor to record only when user is found on request

--- client/src/utils/authAxios.js
+++ client/src/utils/authAxios.js
@@ -16,9 +16,7 @@ export function AxiosInterceptor(unauth, hideRoutes) {
 
 export function loginInterceptor(setProtectedRoutes) {
   axios.interceptors.response.use((response) => {
-    console.log(response.data)
     if (response.data.user) {
-      console.log(response.data)
       storeUser(response.data.user)
       setProtectedRoutes()
     }
