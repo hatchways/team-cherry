d3fd07d8cafbd9d31659257337682fb3c3b70654
Brian <a.gray.cloud@gmail.com>
2020-07-09 21:21:02 -0400

    redo mention getter route

--- server/routes/mentions.js
+++ server/routes/mentions.js
@@ -7,32 +7,39 @@ const { Mention, User } = require("../models");
 // TODO test mentions in action
 
 router.get("/", requiresAuth, async (req, res) => {
+  let output = [];
+  const { keywords, platforms } = req.body;
+  const searchQuery = `%${keywords}%`;
+
   const user = await User.findByPk(req.user.id);
-
-  // get companies associated with the user
   const companies = await user.getCompanies();
-
-  // get mentions associated with the companies
-  output = [];
   for (let company of companies) {
-    let mentions = await company.getMentions();
+    let mentions = await company.getMentions({
+      where: {
+        // note: passing the array directly into `where` will implicitly use [Op.in]
+        // is the mention from one of the toggled platforms?
+        platform: platforms,
+
+        // is the search query on the title or content?
+        [Op.or]: [
+          {
+            title: {
+              [Op.like]: searchQuery,
+            },
+          },
+          {
+            content: {
+              [Op.like]: searchQuery,
+            },
+          },
+        ],
+      },
+    });
+
     output = output.concat(mentions);
   }
 
   res.json({ mentions: output });
-});
-
-router.get("/search/:query", requiresAuth, async (req, res) => {
-  const mentions = await Mention.findAll({
-    where: {
-      title: {
-        // checks if the search queried exists at some point in the title
-        [Op.iLike]: "%" + req.params.query + "%",
-      },
-    },
-  });
-
-  res.json(mentions);
 });
 
 router.get("/email-list", async (req, res) => {
