5e1b8e2fdf393efafea94ce385ce37a78bdb2dc5
Brian <a.gray.cloud@gmail.com>
2020-07-15 08:30:52 -0400

    redo backend mentions without cache

--- server/routes/mentions.js
+++ server/routes/mentions.js
@@ -1,7 +1,5 @@
 const router = require("express").Router();
 const { Op } = require("sequelize");
-const NodeCache = require("node-cache");
-const mentionsCache = new NodeCache();
 
 const requiresAuth = require("./middleware/requiresAuth");
 const { Mention, User } = require("../models");
@@ -16,58 +14,52 @@ router.get("/", requiresAuth, async (req, res) => {
     keywords = "";
   }
 
+  //TODO Do pagination without cache, use original idea(counter for page size and total mentions fetched so far)
+
   const searchQuery = `%${keywords}%`;
   page = parseInt(page) || 1;
 
   const user = await User.findByPk(req.user.id);
   const companies = await user.getCompanies();
 
-  let cacheKey = "keywords=" + keywords + "&platforms=" + platforms.join(",");
-  let companyNames = companies.map((c) => c.name);
-  let output = tryToGetFromCache(mentionsCache, cacheKey, companyNames);
+  // pagination variables
+  const pageSize = Math.ceil(20 / companies.length);
+  const offset = (page - 1) * pageSize;
+  const limit = pageSize;
 
-  if (!output) {
-    console.log("cache reset");
-    output = [];
-    for (let company of companies) {
-      let mentions = await company.getMentions({
-        where: {
-          // is the mention from one of the toggled platforms?
-          platform: platforms,
+  output = [];
+  for (let company of companies) {
+    let mentions = await company.getMentions({
+      limit,
+      offset,
+      where: {
+        // is the mention from one of the toggled platforms?
+        platform: platforms,
 
-          // is the search query on the title or content?
-          [Op.or]: [
-            {
-              title: {
-                [Op.like]: searchQuery,
-              },
+        // is the search query on the title or content?
+        [Op.or]: [
+          {
+            title: {
+              [Op.like]: searchQuery,
             },
-            {
-              content: {
-                [Op.like]: searchQuery,
-              },
+          },
+          {
+            content: {
+              [Op.like]: searchQuery,
             },
-          ],
-        },
-      });
-
-      output = output.concat(mentions);
-    }
-    // TODO sort output based on date
-    output.sort((a, b) => Date.parse(a.date) + Date.parse(b.date));
-    mentionsCache.set(cacheKey, output);
-    mentionsCache.set("key", cacheKey);
-    mentionsCache.set("companies", companyNames);
+          },
+        ],
+      },
+    });
+    output = output.concat(mentions);
   }
+  // TODO sort output based on date
+  // output.sort((a, b) => Date.parse(a.date) + Date.parse(b.date));
 
-  // console.log(output.map((c) => c.date));
-  const pageSize = 5; // you can change this value
-  const offset = page * pageSize;
-  const chunk = output.slice(offset - pageSize, offset);
-  const hasMore = offset <= output.length;
-  const count = chunk.length;
+  const count = output.length;
+  const hasMore = count ? true : false;
 
-  res.json({ page, hasMore, count, mentions: chunk });
+  res.json({ page, hasMore, count, mentions: output });
 });
 
 router.get("/email-list", async (req, res) => {
