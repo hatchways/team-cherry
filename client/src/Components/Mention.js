0c9bd387433d5e7cc1b137950624e994a0ec7da8
Brian <a.gray.cloud@gmail.com>
2020-07-21 16:16:38 -0400

    space like icon to top right corner of mention

--- client/src/Components/Mention.js
+++ client/src/Components/Mention.js
@@ -1,6 +1,8 @@
 import React from "react";
+import axios from "axios";
 import { makeStyles } from "@material-ui/core/styles";
-import { Typography } from "@material-ui/core";
+import { Typography, IconButton } from "@material-ui/core";
+import FavoriteIcon from "@material-ui/icons/Favorite";
 
 const useStyles = makeStyles(() => ({
   Card: {
@@ -38,10 +40,26 @@ const useStyles = makeStyles(() => ({
   fontColorForPlatform: {
     color: "#D3D3D3",
   },
+  header: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  likeIcon: {
+    position: "relative",
+    bottom: "10px",
+  },
 }));
 
 export default function Mention(props) {
   const classes = useStyles();
+  const toggleLike = async (mentionId) => {
+    const res = await axios.post(`/api/users/mentions/${mentionId}/like`);
+    const { mention } = res.data;
+    if (!mention.liked) {
+      // might use a context dispatch here to signal to LikedMentions component that a mention is possibly unliked
+      console.log(`mention ${mention.id} was unliked`);
+    }
+  };
 
   return (
     <div className={classes.Card}>
@@ -49,20 +67,28 @@ export default function Mention(props) {
         {props.image ? (
           <img className={classes.thumbnailImg} src={props.image} alt="" />
         ) : (
-            <img
-              className={classes.thumbnailImg}
-              src={`/imgs/${props.platform}_icon.png`}
-              alt=""
-            />
-          )}
+          <img
+            className={classes.thumbnailImg}
+            src={`/imgs/${props.platform}_icon.png`}
+            alt=""
+          />
+        )}
       </div>
 
       <div className={classes.dividerDiv}></div>
 
       <div className={classes.contentDiv}>
-        <Typography noWrap gutterBottom variant="h5" component="h2">
-          {props.title}
-        </Typography>
+        <div className={classes.header}>
+          <Typography noWrap gutterBottom variant="h5" component="h2">
+            {props.title}
+          </Typography>
+          <IconButton
+            className={classes.likeIcon}
+            onClick={() => toggleLike(props.id)}
+          >
+            <FavoriteIcon />
+          </IconButton>
+        </div>
 
         <Typography
           gutterBottom
@@ -74,6 +100,7 @@ export default function Mention(props) {
         </Typography>
 
         <p className={classes.paragraphInMentions}>{props.content}</p>
+        <div className={classes.dividerDiv}></div>
       </div>
     </div>
   );
