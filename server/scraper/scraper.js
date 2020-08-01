9a35505d2f8283713e0daaee5ff01f1a85d7afdb
Brian <a.gray.cloud@gmail.com>
2020-08-01 14:41:33 -0400

    remove console log call

--- server/scraper/scraper.js
+++ server/scraper/scraper.js
@@ -3,7 +3,7 @@ const {
   twitterCrawler,
   nytCrawler,
   newsApiCrawler,
-} = require('../crawlers');
+} = require("../crawlers");
 
 module.exports = function ScraperManager() {
   this.run = async function run(companyName) {
@@ -16,9 +16,7 @@ module.exports = function ScraperManager() {
     // const twitterMentions = await twitterCrawler(companyName);
     const twitterMentions = [];
     const nytMentions = await nytCrawler(companyName);
-    console.log('calling news api crawler');
     const newsApiMentions = await newsApiCrawler(companyName);
-    console.log('finished newsApi crawler');
     results = results.concat(
       redditMentions,
       twitterMentions,
